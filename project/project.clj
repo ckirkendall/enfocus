c6653018f86b3e21c3e3712e0f032cf010a1315a
ckirkendall <ckirkendall@gmail.com>
2011-12-20 13:48:41 -0500

    fixed diirectory structure to work with the new cljs-watch

--- project/project.clj
+++ project/project.clj
@@ -1,5 +1,6 @@
 (defproject enfocus "0.1.0-SNAPSHOT"
   :description "DOM manipulation tool for clojurescript inspired by Enlive"
-  :resources-path "cljs-src"
+  :source-path "cljs-src"
+  :resources-path "cljs-macros"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]])
