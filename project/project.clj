3888c6ade3d1fbafbd1b2f7d0c1492aa5947ae37
ckirkendall <ckirkendall@gmail.com>
2012-01-02 16:43:22 -0500

    adding goog-jar.jar as a dependency and updated the contribute instructions

--- project/project.clj
+++ project/project.clj
@@ -2,5 +2,6 @@
   :description "DOM manipulation tool for clojurescript inspired by Enlive"
   :source-path "cljs-src"
   :resources-path "cljs-macros"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [goog-jar "1.0.0"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]])
