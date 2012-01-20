912f64f4b5e8275eca617961562007d61c04a304
ckirkendall <ckirkendall@gmail.com>
2012-01-20 11:27:26 -0500

    Moving build setup to lein-cljsbuild

--- project/project.clj
+++ project/project.clj
@@ -1,7 +1,17 @@
 (defproject enfocus "0.9.0-SNAPSHOT"
   :description "DOM manipulation tool for clojurescript inspired by Enlive"
-  :source-path "cljs-src"
-  :resources-path "cljs-macros"
+  :souce-path "cljs-src"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [goog-jar "1.0.0"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]])
+  :dev-dependencies [[lein-eclipse "1.0.0"]
+                     [lein-cljsbuild "0.0.7"]]
+  :cljsbuild {
+    ; The path to the top-level ClojureScript source directory:
+    :source-path "cljs-src"
+    ; The standard ClojureScript compiler options:
+    ; (See the ClojureScript compiler documentation for details.)
+    :compiler {
+      :output-dir "../sample/resources/public/cljs" 
+      :output-to "../sample/resources/public/cljs/enfocus.js"         
+      :optimizations :whitespace
+      :pretty-print true}})
