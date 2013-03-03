9e668390c8cc41fd5b5be076c0098632c3af36d3
ckirkendall <ckirkendall@gmail.com>
2013-03-03 16:13:02 -0500

    validating lein-cljsbuild 0.3.0

--- project/project.clj
+++ project/project.clj
@@ -3,12 +3,11 @@
   :source-path "cljs-src"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [domina "1.0.1" :exclusions [org.clojure/clojurescript]]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]
-                     [lein-cljsbuild "0.2.10"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
   :cljsbuild
   {:builds
    [{:builds nil,
-     :source-path "cljs-src",
+     :source-paths ["cljs-src"],
      :compiler
      {:output-dir "../testing/resources/public/cljs",
       :output-to "../testing/resources/public/cljs/enfocus.js",
