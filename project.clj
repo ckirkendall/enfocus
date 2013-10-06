bc8244695eecd80df53e8dbe2d76edb7365506c8
magomimmo <mimmo.cosenza@gmail.com>
2013-10-06 16:01:22 +0200

    added clojurescript.test

--- project.clj
+++ project.clj
@@ -4,25 +4,62 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
+
+  :min-lein-version "2.1.2"
+  
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1913"]
+                 [com.cemerick/clojurescript.test "0.0.4"]
                  [domina "1.0.2"]
                  [org.jsoup/jsoup "1.7.2"]]
 
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "0.3.3"]]
   :hooks [leiningen.cljsbuild]
 
   :cljsbuild
   {:crossovers [enfocus.enlive.syntax]
    :crossover-jar true
 
-   :builds
-   [{:builds nil,
-     :source-paths ["src/cljs" "test/cljs"]
-     :compiler
-     {:output-to "dev-resources/public/js/enfocus.js"
-      :optimizations :whitespace
-      :pretty-print true}}]})
+   :builds {:none
+            {:source-paths ["src/cljs" "test/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/none.js"
+              :optimizations :none
+              :pretty-print true}}
+
+            :whitespace
+            {:source-paths ["src/cljs" "test/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/whitespace.js"
+              :optimizations :whitespace
+              :pretty-print true}}
+            
+            :simple
+            {:source-paths ["src/cljs" "test/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/simple.js"
+              :optimizations :simple
+              :pretty-print false}}
+
+            :advanced
+            {:source-paths ["src/cljs" "test/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/advanced.js"
+              :optimizations :advanced
+              :pretty-print false}}}
+
+   :test-commands {"none"
+                   ["runners/phantomjs.js" "dev-resources/public/js/none.js"]
+
+                   "whitespace"
+                   ["runners/phantomjs.js" "dev-resources/public/js/whitespace.js"]
+
+                   "simple"
+                   ["runners/phantomjs.js" "dev-resources/public/js/simple.js"]
+
+                   "advanced"
+                   ["runners/phantomjs.js" "dev-resources/public/js/advanced.js"]}})
 
