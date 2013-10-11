b9e436b7352e175b698f78714f42538c7907fd9f
Magomimmo <mimmo.cosenza@gmail.com>
2013-10-11 15:22:12 +0200

    added clojurescript.test

--- project.clj
+++ project.clj
@@ -4,64 +4,50 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-
+  
   :min-lein-version "2.1.2"
-
-  :clojurescript? true
-
+  
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-
+  
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1913"]
+                 [com.cemerick/clojurescript.test "0.0.4"]
                  [domina "1.0.2"]
                  [org.jsoup/jsoup "1.7.2"]]
-
+  
   :plugins [[lein-cljsbuild "0.3.3"]]
-
+  
   :hooks [leiningen.cljsbuild]
-
+  
   :cljsbuild
   {:crossovers [enfocus.enlive.syntax]
-   :crossover-jar true
-
-   :builds {:deploy 
-            {:source-paths ["src/cljs"]
-             :jar true
+   
+   :builds {:whitespace
+            {:source-paths ["src/cljs" "test/cljs"]
              :compiler
-             {:output-to "dev-resources/public/js/enfocus.js"
+             {:output-to "dev-resources/public/js/whitespace.js"
+              :optimizations :whitespace
+              :pretty-print true}}
+             
+            :simple 
+            {:source-paths ["src/cljs" "test/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/simple.js"
+              :optimizations :simple
+              :pretty-print false}}
+             
+            :advanced
+            {:source-paths ["src/cljs" "test/cljs"]
+             :compiler
+             {:output-to "dev-resources/public/js/advanced.js"
               :optimizations :advanced
-              :pretty-print false}}}}
-  
-  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]]
-                   :cljsbuild 
-                   {:builds {:whitespace
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/whitespace.js"
-                               :optimizations :whitespace
-                               :pretty-print true}}
-
-                             :simple 
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/simple.js"
-                               :optimizations :simple
-                               :pretty-print false}}
-
-                             :advanced
-                             {:source-paths ["src/cljs" "test/cljs"]
-                              :compiler
-                              {:output-to "dev-resources/public/js/advanced.js"
-                               :optimizations :advanced
-                               :pretty-print false}}}
-                    
-                    :test-commands 
-                    {"whitespace"
-                     ["runners/phantomjs.js" "dev-resources/public/js/whitespace.js"]
+              :pretty-print false}}}
+   :test-commands {"whitespace"
+                   ["runners/phantomjs.js" "dev-resources/public/js/whitespace.js"]
                    
-                     "simple"           
-                     ["runners/phantomjs.js" "dev-resources/public/js/simple.js"]
-                                    
-                     "advanced"
-                     ["runners/phantomjs.js" "resources/public/js/advanced.js"]}}}})
+                   "simple"           
+                   ["runners/phantomjs.js" "dev-resources/public/js/simple.js"]
+                   
+                   "advanced"
+                   ["runners/phantomjs.js" "resources/public/js/advanced.js"]}})
