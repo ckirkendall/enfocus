02719e73a9106271a3e183ed17704b5593f9289f
magomimmo <mimmo.cosenza@gmail.com>
2013-10-14 00:22:56 +0200

    cljs downgraded to 1835

--- project.clj
+++ project.clj
@@ -11,10 +11,9 @@
   :test-paths ["test/clj"]
   
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1913"]
+                 [org.clojure/clojurescript "0.0-1835"]
                  [domina "1.0.2"]
-                 [org.jsoup/jsoup "1.7.2"]
-                 [compojure "1.1.5"]]
+                 [org.jsoup/jsoup "1.7.2"]]
   
   :plugins [[lein-cljsbuild "0.3.4"]]
   
