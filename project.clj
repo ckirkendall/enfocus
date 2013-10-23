85017b365efad79e07e591335ba9b23759c27f51
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-10-23 14:25:21 +0200

    added slimerjs test-commands

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  
+
   :min-lein-version "2.2.0"
-  
+
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1847"]
                  [domina "1.0.2"]
                  [org.jsoup/jsoup "1.7.2"]]
-  
+
   :plugins [[lein-cljsbuild "0.3.4"]]
-  
+
   :hooks [leiningen.cljsbuild]
-  
+
   :cljsbuild
   {:crossovers [enfocus.enlive.syntax]
    :crossover-jar true
@@ -32,21 +32,21 @@
               :pretty-print true}}}}
 
   :profiles {:dev {:resources-paths ["dev-resources"]
-                   
+
                    :dependencies [[ring "1.2.0"]
                                   [compojure "1.1.5"]
                                   [com.cemerick/piggieback "0.1.0"]]
-                   
+
                    :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
-                   
-                   :cljsbuild 
+
+                   :cljsbuild
                    {:builds {:whitespace
                              {:source-paths ["src/cljs" "test/cljs" "src/brepl"]
                               :compiler
                               {:output-to "dev-resources/public/js/whitespace.js"
                                :optimizations :whitespace
                                :pretty-print true}}
-                             
+
                              :simple
                              {:source-paths ["src/cljs" "test/cljs" "src/brepl"]
                               :compiler
@@ -60,21 +60,37 @@
                               {:output-to "dev-resources/public/js/advanced.js"
                                :optimizations :advanced
                                :pretty-print false}}}
-                    :test-commands {"whitespace"
+
+                    :test-commands {"slimerjs-ws"
+                                    ["slimerjs"
+                                     :runner
+                                     "dev-resources/public/js/whitespace.js"]
+
+                                    "slimerjs-simple"
+                                    ["slimerjs"
+                                     :runner
+                                     "dev-resources/public/js/simple.js"]
+
+                                    "slimerjs-advanced"
+                                    ["slimerjs"
+                                     :runner
+                                     "dev-resources/public/js/advanced.js"]
+
+                                    "phantomjs-ws"
                                     ["phantomjs"
                                      :runner
                                      "dev-resources/public/js/whitespace.js"]
-                                    
-                                    "simple"
+
+                                    "phantomjs-simple"
                                     ["phantomjs"
                                      :runner
                                      "dev-resources/public/js/simple.js"]
-                                    
-                                    "advanced"
-                                    ["phantomjs" 
+
+                                    "phantomjs-advanced"
+                                    ["phantomjs"
                                      :runner
                                      "dev-resources/public/js/advanced.js"]}}
-                               
+
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :injections [(require '[cljs.repl.browser :as brepl]
                                          '[cemerick.piggieback :as pb])
