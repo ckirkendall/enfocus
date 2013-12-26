4245f8ad3b84b85a768c6cfe31e8689852cfb060
ckirkendall <ckirkendall@gmail.com>
2013-12-26 14:56:55 -0500

    added some helper functions and tests for future binding work.

--- src/cljs/enfocus/bind.cljs
+++ src/cljs/enfocus/bind.cljs
@@ -1,6 +1,52 @@
 (ns enfocus.bind
   (:require
-   [enfocus.core :as ef :refer [from at set-attr get-attr]]))
+   [enfocus.core :as ef :refer [from at set-attr get-attr]]
+   [goog.object :as gobj]))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  HELPER FUNCTIONS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def default-bindings-opts {:type :one-way
+                            :event :onblur
+                            :field nil})
+
+
+(defn mget-in
+  "gets a property in a complex obj or map. This function is similar
+   to get-in except that it can get properties of objects and maps
+   at any layer in the object, by passing in a seq as the field list
+   [l1 l2 l3]."
+  [obj field]
+  (cond
+   (and (map? obj) (sequential? field)) (get-in obj field val)
+   (map? obj) (obj field)
+   (and obj (keyword? field )) (aget obj (name field))
+   (and obj (string? field)) (aget obj field)
+   (and obj (sequential? field)) (apply aget obj (map name field))
+   :else nil))
+ 
+(defn mset-in
+  "sets a property in a complex obj or map. This function is similar
+   to assoc-in except that it can set properties of objs and maps
+   at any layer in the object, by passing in a seq as the field list
+   [l1 l2 l3]."
+  [obj field val]
+  (cond
+   (and (map? obj) (sequential? field)) (assoc-in obj field val)
+   (map? obj) (assoc obj field val)
+   (and obj (keyword? field )) (do (aset obj (name field) val) obj)
+   (and obj (string? field)) (do (aset obj field val) obj)
+   (and obj (sequential? field)) (apply aset obj
+                                        (concat (map name field) [val]))
+   :else obj))
+ 
+(defn key-or-props [obj]
+  (if (map? obj)
+    (keys obj)
+    (seq (gobj/getKeys obj))))
+
+
 
 (defn- build-key [id]
   (str "__EVB:" id))
--- test/cljs/enfocus/bind_test.cljs
+++ test/cljs/enfocus/bind_test.cljs
@@ -1,7 +1,7 @@
 (ns enfocus.bind-test
   (:require
    [enfocus.core :as ef :refer [at from content get-text]]
-   [enfocus.bind :as bind :refer [bind-view]]
+   [enfocus.bind :as bind :refer [bind-view mget-in mset-in key-or-props]]
    [cemerick.cljs.test :as t])
   (:require-macros
    [enfocus.macros :as em]
@@ -9,12 +9,9 @@
 
 (defn each-fixture [f]
   ;; initialize the environment
-  (let [div (.createElement js/document "div")
-        pc (.createElement js/document "p")]
+  (let [div (.createElement js/document "div")]
     (.setAttribute div "id" "test-id")
     (.setAttribute div "class" "test-class")
-    (.setAttribute pc "class" "test-class")
-    (.appendChild div pc)
     (.appendChild (.-body js/document) div)
     ;; execute the unit test
     (f)
@@ -23,6 +20,43 @@
 
 (use-fixtures :each each-fixture)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HELPER FUNC TESTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest mget-in-test
+  (testing "standard js-obj object"
+    (let [obj (js-obj "a" (js-obj "c" 3) "b" 4)]
+      (testing "simple field"
+        (are [expected actual] (= expected actual)
+             4 (mget-in obj "b")
+             4 (mget-in obj :b)))
+      (testing "multi leve acess"
+        (are [expected actual] (= expected actual)
+             3 (mget-in obj ["a" "c"])
+             3 (mget-in obj [:a :c])
+             3 (mget-in obj [:a "c"])
+             3 (mget-in obj ["a" :c])))))
+  (testing "standard clojurescript map"
+    (let [obj {:a {:b 3 :c 4 :d {:a 5}} :b 2}]
+      (testing "simple field"
+        (is (= 2 (mget-in obj :b))))
+      (testing "multi leve acess"
+        (are [expected actual] (= expected actual)
+             3 (mget-in obj [:a :b])
+             5 (mget-in obj [:a :d :a]))))))
+
+(deftest key-or-prop-test
+  (testing "getting the keys from an obj or map"
+    (is (= [:a :b] (key-or-props {:a 2 :b 3})))
+    (is (= ["a" "b"] (key-or-props (js-obj "a" 2 "b" 3))))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   MAIN TESTS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (deftest bind-view-test
   (testing "binding a view to an atom"
     (let [atm (atom "initial")]
